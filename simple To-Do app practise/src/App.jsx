aef8edd4a8395212770d8c7ad9b7a6f5c1b72a80
NafisFuadTanvir <nafisf026@gmail.com>
2023-08-24 23:14:17 +0600

    practise useref and creating a simple todo using use state

---
+++ simple To-Do app practise/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState("");
+
+
+
+  const addTolist = () => {
+    list.push(item);
+
+    setList([...list]);
+         
+    
+  };
+
+
+  const removeItem=(index)=>{
+
+      
+    list.splice(index,1);
+
+    setList([...list])
+
+
+
+  }
+
+  return (
+    <div>
+ 
+
+  <h1>My simple To do App</h1>
+      <table>
+        <tbody>
+
+          {list.length !== 0 ? (
+
+            list.map((listitem, index) => {
+              return (
+                <tr>
+                  <td>{listitem}</td>
+
+                  <td>
+                    <button onClick={(index)=>{ removeItem(index)}}>Remove</button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr></tr>
+          )}
+        </tbody>
+      </table>
+
+      <input
+        onChange={(event) => {
+          setItem(event.target.value);
+        } }
+        placeholder="Enter the item"
+      />
+
+      <button onClick={addTolist}>Add</button>
+    </div>
+  );
+}
+
+export default App;
